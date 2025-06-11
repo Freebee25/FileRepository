36034931509eff795dfb46813fef2ce18f2e3cc6
Freebee <fribyany6661@gmail.com>
2025-06-11 11:38:53 +0700

    fix mailer

--- helpers/mailer.php
+++ helpers/mailer.php
@@ -11,12 +11,12 @@ function sendMail($to, $subject, $message)
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'apnverify@gmail.com';
-        $mail->Password = 'fvqvinfichkznpat';
+        $mail->Username = 'fribyany6661@gmail.com';
+        $mail->Password = 'hujxnovkkxpkrjji';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
-        $mail->setFrom('apnverify@gmail.com', 'Verification email');
+        $mail->setFrom('fribyany6661@gmail.com', 'Verification email');
         $mail->addAddress($to);
         $mail->addReplyTo('no-reply@gmail.com', 'No Reply');
 
