2af72e61f2f70be3969fec0e7ce931af63356716
Freebee <fribyany6661@gmail.com>
2025-06-11 12:57:48 +0700

    tambah nama user di navbar

--- resource/template/navbar.php
+++ resource/template/navbar.php
@@ -8,12 +8,18 @@
     <link rel="stylesheet" href="../fontawesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/navbar-hover.css">
     <script src="../js/bootstrap.bundle.min.js"></script>
+
+    <style>
+        .dropdown-menu.show {
+            display: block;
+        }
+    </style>
 </head>
+
 <body>
-    <header class="bg-light text-white p-3">
+    <header class="bg-light p-3 border-bottom">
         <div class="container d-flex justify-content-between align-items-center">
-            <div class="d-flex align-items-center">
-            </div>
+            <!-- Navigation Menu -->
             <nav>
                 <ul class="nav">
                     <li class="nav-item">
@@ -27,8 +33,41 @@
                     </li>
                 </ul>
             </nav>
-            <a href="../../controllers/logout.php" class="btn btn-light">
-                <i class="fa fa-sign-out"></i> Logout</a>
+
+            <!-- User Dropdown -->
+            <div class="dropdown">
+                <button onclick="toggleDropdown()" id="userDropdown" class="btn btn-light dropdown-toggle" type="button">
+                    <i class="fa fa-user"></i> <?php echo htmlspecialchars($_SESSION['username']); ?>
+                </button>
+                <div id="dropdownMenu" class="dropdown-menu dropdown-menu-end mt-2 shadow-sm">
+                    <a class="dropdown-item" href="../views/manageakun.php">
+                        <i class="fa fa-cog me-2"></i> Manage Akun
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item text-danger" href="../../controllers/logout.php">
+                        <i class="fa fa-sign-out me-2"></i> Logout
+                    </a>
+                </div>
+            </div>
         </div>
     </header>
+
+    <script>
+        function toggleDropdown() {
+            const menu = document.getElementById('dropdownMenu');
+            menu.classList.toggle('show');
+        }
+
+        // Tutup dropdown jika klik di luar
+        window.addEventListener('click', function (e) {
+            const button = document.getElementById('userDropdown');
+            const menu = document.getElementById('dropdownMenu');
+
+            if (!button.contains(e.target) && !menu.contains(e.target)) {
+                menu.classList.remove('show');
+            }
+        });
+    </script>
 </body>
+
+</html>
